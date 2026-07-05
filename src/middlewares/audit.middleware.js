62cd79ed2e42879ace64c0098b17427d0439a47e
Zamu-ai <uriel.b.zambrano@gmail.com>
2026-07-04 21:21:44 -0300

    sistema de auditoria de acciones y controles de acceso

---
+++ src/middlewares/audit.middleware.js
@@ -0,0 +1,32 @@
+//este middleware va a "escuchar" las peticiones y las registra
+const AuditLog= require('../models/audit.model')
+
+const auditMiddleware = (accion)=>{  //recibe el nombre de la accion
+    return async(requestAnimationFrame,resizeBy,next)=>{  //retorna el middleware
+        //voy a guardar la respuesta original "res.send" que guarda las respuestas qu da laBD al realizar una accion
+        const rtaOriginal =res.send
+        
+        res.send = function(data){//sobreescribimos "res.send" para interceptar la respuesta
+            try{ //en este momento la BD ya respondió a la accion que realizaste
+                const usuario= req.user ? req.user.username : 'ANONIMO' //si el usuario es autenticado guardamos su nombre
+                AuditLog.create({  //creamos el registro de Auditoria
+                    usuario:usuario,
+                    accion:accion,
+                    metodo:req.method,
+                    ruta:req.originalUrl,
+                    ip:req.ip,
+                    userAgent:req.headers['user-agent'], //chrome,firefox...
+                    fecha:new Date(),
+                    statusCode:res.statusCode, //el status de la rta 404,505,200
+                    resultado:data  //lo que devolvió
+                }).catch(err => console.error('Error al guardar auditoria',err))
+            } catch(err){
+              console.error('Error en auditoría:', err)
+            }
+            //se llama a la funcion original para enviar la respuesta 
+            rtaOriginal.call(this,data)
+        }
+        next() //se continua con el siguiente middleware
+    }
+}
+module.exports=auditMiddleware
--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,8 +1,9 @@
 const funcionLogin=require('../controllers/login.controller')
+const auditoriaMiddleware=require('../middlewares/audit.middleware')
 const express=require('express')
 const router=express.Router()
 
-router.post('/',funcionLogin.createUsuario)
-router.post('/loginUser',funcionLogin.loginUsuario)
+router.post('/',auditoriaMiddleware('CREAR_USUARIO'),funcionLogin.createUsuario)
+router.post('/loginUser',auditoriaMiddleware('LOGUEAR_USUARIO'),funcionLogin.loginUsuario)
 
 module.exports=router
